cd4eb43b2cfcac136614016fa1d68c8c74d25d02
cmtheit <cmtheit@outlook.com>
2023-04-18 22:59:35 +0800

    init

---
+++ fe/src/components/header/Header.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import Logo from "@/components/header/logo/Logo.vue";
+</script>
+
+<template>
+<Logo />
+</template>
+
+<style scoped>
+
+</style>
---
+++ fe/src/components/header/logo/Logo.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+    <div class="self">
+        <img src="/assets/logo.jpg" alt="公司Logo" />
+    </div>
+</template>
+
+<style scoped lang="scss">
+.self {
+    img {
+        height: 3em;
+    }
+}
+</style>
