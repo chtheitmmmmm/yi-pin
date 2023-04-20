4f3fcca87039af6e10645bd127b69aa2965c78db
cmtheit <cmtheit@outlook.com>
2023-04-20 16:22:57 +0800

    server init

--- yi-pin-be/src/app.module.ts
+++ yi-pin-be/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApiController } from './api/api.controller';
+import { ApiService } from './api/api.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule],
+  controllers: [AppController, ApiController],
+  providers: [AppService, ApiService],
 })
 export class AppModule {}
